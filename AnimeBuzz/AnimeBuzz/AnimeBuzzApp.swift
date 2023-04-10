d031633bc46a5f0772c36051949d5b2a8d6855f8
ThiagoDefini <thiagod.scherer@hotmail.com>
2023-04-10 16:45:25 -0300

    Commit inicial com a pasta do projeto e gitignore

---
+++ AnimeBuzz/AnimeBuzz/AnimeBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeBuzzApp.swift
+//  AnimeBuzz
+//
+//  Created by Thiago Defini on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
