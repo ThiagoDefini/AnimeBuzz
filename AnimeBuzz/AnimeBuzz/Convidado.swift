7f8044922fa4e616e6c3e06eedce6863a8f89cf4
Maria Luísa Lamb Souto <maria@Maria-Luisa-MacBook-Pro.local>
2023-04-10 17:06:15 -0300

    Testando primeiras alterações

---
+++ AnimeBuzz/AnimeBuzz/Convidado.swift
@@ -0,0 +1,21 @@
+//
+//  Convidado.swift
+//  AnimeBuzz
+//
+//  Created by Maria Luísa Lamb Souto on 10/04/23.
+//
+
+import SwiftUI
+
+struct Convidado: View {
+    var body: some View {
+        Text("Convidados")
+            .bold()
+    }
+}
+
+struct Convidado_Previews: PreviewProvider {
+    static var previews: some View {
+        Convidado()
+    }
+}
