68dc30a967bb18f9854035027948bb11e5dc22bf
Mateus Godinho <mat11godinho@hotmail.com>
2023-04-11 17:26:43 -0300

    Cores e TesteView atualizados
    AccentColor virou branco e o amarelo agora é gold, sob o mesmo nome de yellow.
    
    TesteView com navigation bar nova

--- AnimeBuzz/AnimeBuzz/TesteView.swift
+++ AnimeBuzz/AnimeBuzz/TesteView.swift
@@ -12,16 +12,30 @@ struct TesteView: View {
         NavigationStack{
             ZStack{
                 Colors().black.edgesIgnoringSafeArea(.top)// BARRA TOPO
+                Colors().pink.edgesIgnoringSafeArea(.bottom).padding(.top, 40)                
                 
-                VStack{
-                    Images().logo.resizable().frame(width:80, height:60).padding(.bottom, 10)
-                    Colors().pink.edgesIgnoringSafeArea(.bottom)
-                } // CORPO DO APP
-                
+                //COMEÇO DO CORPO DA VIEW--
                 Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).foregroundColor(.white)
-                
             }
-        }.navigationBarBackButtonHidden()
+        }
+//        .navigationBarBackButtonHidden()
+        .toolbar{
+            ToolbarItem(placement: .principal){
+                Images().logo
+                    .resizable()
+                    .padding(.top, 40.0)
+                    .frame(width: 100.0, height: 100.0)
+            }
+            
+            ToolbarItemGroup(placement: ToolbarItemPlacement.navigationBarTrailing){
+                Button(action:{}, label:{
+                    Label("Send", systemImage: "info.circle").foregroundColor(Color.white)
+                })
+                Button(action:{}, label:{
+                    Label("Send", systemImage: "at").foregroundColor(Color.white)
+                })
+            }
+        }
     }
 }
 
