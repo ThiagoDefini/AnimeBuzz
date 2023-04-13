d7b273c3263fa28ce3501e94307d8753805b5918
Felipe Araujo <felipearaujo@10-41-49-81.0200.epoa.br.iosda.org>
2023-04-13 15:28:44 -0300

    adicionei tela de Atracao e mudei a classe attraction

---
+++ AnimeBuzz/AnimeBuzz/AttractionView.swift
@@ -0,0 +1,77 @@
+//
+//  AttractionView.swift
+//  AnimeBuzz
+//
+//  Created by Felipe Araujo on 13/04/23.
+//
+
+import SwiftUI
+
+
+struct AttractionView: View {
+    @State var attraction: Attraction
+    var body: some View {
+            ZStack{
+                Colors().pink
+                    .ignoresSafeArea()
+                ScrollView{
+                VStack{
+                    ZStack{
+                        Images().banner
+                        Text(attraction.getName())
+                            .foregroundColor(Colors().black)
+                            .font(.title)
+                    }
+                        .padding(.bottom, padding2)
+                    Text(attraction.getDescription())
+                        .foregroundColor(Colors().principal)
+                        .padding(.bottom, padding2)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: radius)
+                            .fill(Colors().yellow)
+                            .frame(width: 200, height: 50)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: radius)
+                                    .stroke(Color.black, lineWidth: borderWidth))
+                        HStack{
+                            Icons().estrela
+                                .foregroundColor(Colors().principal)
+                            Text("Favoritar")
+                                .font(.title2)
+                                .foregroundColor(Colors().principal)
+                        }
+                    }
+                    .padding(.bottom, padding2)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: radius)
+                            .fill(Colors().blue)
+                            .frame(width: 350, height: 70)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: radius)
+                                    .stroke(Color.black, lineWidth: borderWidth))
+                        Text("\(attraction.getDate()) - \(attraction.getTimeBegin()) até \(attraction.getTimeEnding())")
+                            .font(.title2)
+                            .foregroundColor(Colors().principal)
+                    }
+                    .padding(.bottom, padding2)
+                    Text("Local:")
+                        .font(.title2)
+                        .foregroundColor(Colors().principal)
+                        .padding(.trailing, 290)
+                    Image(attraction.getMapName())
+                        .resizable()
+                        .frame(width: 350, height: 600)
+                        .cornerRadius(radius)
+                }
+            }
+                .scrollIndicators(.hidden)
+                
+        }
+    }
+}
+
+struct AttractionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AttractionView(attraction: testeAtracoes)
+    }
+}
